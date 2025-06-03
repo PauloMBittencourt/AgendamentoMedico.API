9ceb4f9edd4d37f4ea05dd8d862d1ddad610c469
Paulo Medeiros <Paulo.Medeiros@fies.org.br>
2025-06-03 10:24:26 -0300

    Correções no StepFuncionario

--- AgendamentoMedico.Domain/Models/FuncionarioCreateViewModel.cs
+++ AgendamentoMedico.Domain/Models/FuncionarioCreateViewModel.cs
@@ -1,10 +1,4 @@
-﻿using AgendamentoMedico.Domain.Enuns;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AgendamentoMedico.Domain.Models
 {
